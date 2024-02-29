2a5c1217c63780e7966beeb76d602ced6c9959a7
oliver xing <sx45@uw.edu>
2024-02-28 17:18:36 -0800

    Delete secret key cuz I can not figure it out. Update: Sign-in feature, register feature, search feature. I made signin page pretty decent now. If have time, will change the register page. Work next: add sign-in in the homepage navbar at top right. Add security feature

--- mtvsrs/urls.py
+++ mtvsrs/urls.py
@@ -16,10 +16,16 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("register", views.register_user, name="register"),
+    path("", include("django.contrib.auth.urls")),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LoginView.as_view(), name='logout'),
+    path('search/', views.search_feature, name='search_view'),
 ]
--- mtvsrs/views.py
+++ mtvsrs/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from mtvsrs.models import Movie
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.shortcuts import render, redirect
+
+def register_user(request: HttpRequest) -> HttpResponse:
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            success_url = reverse("index")
+            return HttpResponseRedirect(success_url)
+    else:
+        form = UserCreationForm()
+
+    context = {"form": form}
+
+    return render(request, "registration/registration_form.html", context)
 
 
 # @login_required()
@@ -14,4 +32,17 @@ def home(request):
         'movies': movies
     }
     return render(request, "home.html", context)
+
+def search_feature(request):
+    # Check if the request is a post request.
+    if request.method == 'POST':
+        # Retrieve the search query entered by the user
+        search_query = request.POST['search_query']
+        # Filter your model by the search query
+        posts = Movie.objects.filter(name__contains=search_query)
+        return render(request, 'post_search.html', {'query':search_query, 'posts':posts})
+    else:
+        return render(request, 'post_search.html',{})
+
+
 
